e4ce7759e816c7e3734107a2d0b3d22d44b106e3
James Petit <jp.james.petit@gmail.com>
2016-05-11 23:56:00 +0200

    Enemy Follow.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour 
+{
+	public GameObject player;
+	
+	void Update () 
+	{
+		transform.position = Vector3.MoveTowards(transform.position, player.transform.position, Time.deltaTime);
+	}
+}
